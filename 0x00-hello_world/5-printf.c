940f653e036824146798159fbb9cc70e22608513
Judy <rokiymu@uom sers.no 28624631+judreply.github.com>
2023-08-14 02:17:19 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
